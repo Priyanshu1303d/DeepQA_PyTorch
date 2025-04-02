1e19eb519a3870d22a6f4dd1127a9c734d943a0c
Priyanshu1303d <priyanshu1303d@gmail.com>
2025-04-02 11:58:14 +0530

    Data Ingetion Completed

---
+++ src/DeepQA/logging/__init__.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import logging
+
+
+log_string =  "[%(asctime)s : %(levelname)s : %(module)s  : %(message)s]"
+
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir , "running_logs.log")
+os.makedirs(log_dir , exist_ok=True)
+
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = log_string,
+
+    handlers = [
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)  # Uses the module name automatically
+
+
